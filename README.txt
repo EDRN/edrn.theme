c92b980285640f47af7280705cdf48c2e16da023
Sean Kelly <sean.c.kelly@me.com>
2010-01-27 16:31:26 +0000

    0.0.2

--- README.txt
+++ README.txt
@@ -1,4 +1,14 @@
 This is the basic look-and-feel for websites for the Early Detection Research
-Network (EDRN), which loosely follows the style of the National Cancer
-Institute (NCI).  It's also possible, though, that JPL will take the
-look-and-feel in an entirely new direction anyway.
+Network (EDRN_), which loosely follows the style of the National Cancer
+Institute (NCI_).  It was developed by the Informatics Center (IC_), operated
+by JPL_.
+
+Although intended for the EDRN public portal, this package may be used in any
+Plone_ 3 (or later) site.
+
+.. References:
+.. _EDRN: http://edrn.nci.nih.gov/
+.. _IC: http://cancer.jpl.nasa.gov/
+.. _JPL: http://www.jpl.nasa.gov/
+.. _NCI: http://www.cancer.gov/
+.. _Plone: http://plone.org/
--- setup.py
+++ setup.py
@@ -1,36 +1,69 @@
 # encoding: utf-8
-# Copyright 2008 California Institute of Technology. ALL RIGHTS
+# Copyright 2008-2010 California Institute of Technology. ALL RIGHTS
 # RESERVED. U.S. Government Sponsorship acknowledged.
 
 from setuptools import setup, find_packages
-import os
+import os.path
 
-version = '0.0.2'
+# Package data
+# ------------
+
+_name        = 'edrn.theme'
+_version     = '0.0.2'
+_description = 'Look and feel for websites of the Early Detection Research Network (EDRN).'
+_url         = 'http://cancer.jpl.nasa.gov/products/edrn-theme'
+_downloadURL = 'http://oodt.jpl.nasa.gov/dist/edrn-theme'
+_author      = 'Andrew Hart'
+_authorEmail = 'andrew.hart@jpl.nasa.gov'
+_license     = 'Proprietary'
+_namespaces  = ['edrn']
+_entryPoints = {}
+_zipSafe     = False
+_keywords    = 'web zope plone theme edrn cancer biomarkers'
+_requirements = [
+    'setuptools',
+]
+_classifiers = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Web Environment',
+    'Framework :: Plone',
+    'Intended Audience :: Healthcare Industry',
+    'License :: Other/Proprietary License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Scientific/Engineering :: Bio-Informatics',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Software Development :: User Interfaces',
+]
+
+# Setup Metadata
+# --------------
+
+def _read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+_header = '*' * len(_name) + '\n' + _name + '\n' + '*' * len(_name)
+_longDescription = _header + '\n\n' + _read('README.txt') + '\n\n' + _read('docs', 'INSTALL.txt') + '\n\n' \
+    + _read('docs', 'HISTORY.txt') + _read('docs', 'LICENSE.txt')
+open('doc.txt', 'w').write(_longDescription)
 
 setup(
-    name='edrn.theme',
-    version=version,
-    description="Basic look-and-feel for the EDRN websites.",
-    long_description=open("README.txt").read() + "\n" + open(os.path.join("docs", "HISTORY.txt")).read(),
-    # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    keywords='web zope plone theme edrn cancer',
-    author='Andrew Hart',
-    author_email='andrew.hart@jpl.nasa.gov',
-    url='http://cancer.jpl.nasa.gov/products/edrn-theme',
-    download_url='http://oodt.jpl.nasa.gov/dist/edrn-theme',
-    license='Proprietary',
-    packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['edrn'],
+    author=_author,
+    author_email=_authorEmail,
+    classifiers=_classifiers,
+    description=_description,
+    download_url=_downloadURL,
+    entry_points=_entryPoints,
     include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        'setuptools',
-        # -*- Extra requirements: -*-
-    ],
-    entry_points={},
+    install_requires=_requirements,
+    keywords=_keywords,
+    license=_license,
+    long_description=_longDescription,
+    name=_name,
+    namespace_packages=_namespaces,
+    packages=find_packages(exclude=['ez_setup']),
+    url=_url,
+    version=_version,
+    zip_safe=_zipSafe,
 )
