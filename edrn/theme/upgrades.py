a2193acb9bb62353f12c8b0e9d75350a0bd3c41c
Sean Kelly <sean.c.kelly@me.com>
2013-02-19 21:59:27 +0000

    Add upgrade steps for edrn.theme; actually declare skin layer for javasciprts

--- edrn/theme/upgrades.py
+++ edrn/theme/upgrades.py
@@ -2,5 +2,12 @@
 # Copyright 2010 California Institute of Technology. ALL RIGHTS
 # RESERVED. U.S. Government Sponsorship acknowledged.
 
+PROFILE_ID = 'profile-edrn.theme:default'
+
 def nullUpgradeStep(setupTool):
     '''A null step for when a profile upgrade requires no custom activity.'''
+
+def upgrade4to5(setupTool):
+    '''Upgrade 4 to 5 by updating skins and viewlets'''
+    setupTool.runImportStepFromProfile(PROFILE_ID, 'skins')
+    setupTool.runImportStepFromProfile(PROFILE_ID, 'viewlets')
